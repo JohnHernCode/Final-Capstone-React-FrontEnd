362a1f1f970023c901e2a1b506a34236f30b2c50
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-23 10:09:05 -0600

    Add header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BiBarChart } from 'react-icons/bi';
+import { BsCheck } from 'react-icons/bs';
+import { BiPieChartAlt2 } from 'react-icons/bi';
+import { BsThreeDots } from 'react-icons/bs';
+
+const Footer = () => (
+    <footer className="footer">
+      <nav className="footer__nav">
+        <NavLink to="/measure/create" className="footer__nav__item">
+          <BiBarChart />
+          <span className="footer__nav__item__text">Add Measurement</span>
+        </NavLink>
+        <NavLink to="/measures" className="footer__nav__item">
+          <BsCheck />
+          <span className="footer__nav__item__text">Measure List</span>
+        </NavLink>
+        <NavLink to="/progress" className="footer__nav__item">
+          <BiPieChartAlt2 />
+          <span className="footer__nav__item__text">Your Progress</span>
+        </NavLink>
+        <NavLink to="/more" className="footer__nav__item">
+          <BsThreeDots />
+          <span className="footer__nav__item__text">More</span>
+        </NavLink>
+      </nav>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Header = ({ adminUser, whileLoggedIn }) => {
+  const [toggleClass, setToggleClass] = useState('');
+  const onToggleMenu = () => {
+    setToggleClass(toggleClass === ' active' ? '' : ' active');
+  };
+  const closeToggleMenu = () => {
+    setToggleClass('');
+  };
+
+  return (
+      <header className="header">
+        <div className="header__title-wrap">
+          <Link to="/" className="header__title">Study Up!</Link>
+        </div>
+        <button type="button" className={`header__toggle${toggleClass}`} onClick={onToggleMenu}>
+          <span>&nbsp;</span>
+          <span>&nbsp;</span>
+          <span>&nbsp;</span>
+        </button>
+        <nav className={`header__nav${toggleClass}`}>
+          <NavLink exact to="/" className="header__link" onClick={closeToggleMenu}>Home</NavLink>
+          <NavLink exact to="/measure/create" className="header__link" onClick={closeToggleMenu}>Add Measurement</NavLink>
+          <NavLink exact to="/measures" className="header__link" onClick={closeToggleMenu}>All Measurements</NavLink>
+          <NavLink exact to="/progress" className="header__link" onClick={closeToggleMenu}>Progress</NavLink>
+          <NavLink exact to="/more" className="header__link" onClick={closeToggleMenu}>More</NavLink>
+          {!whileLoggedIn && (
+              <>
+                <NavLink exact to="/login" className="header__link" onClick={closeToggleMenu}>Login</NavLink>
+                <NavLink exact to="/signup" className="header__link" onClick={closeToggleMenu}>Signup</NavLink>
+              </>
+          )}
+
+          {(whileLoggedIn && adminUser) && (
+              <>
+                <NavLink exact to="/admin" className="header__link" onClick={closeToggleMenu}>[Admin Menu] Subject List</NavLink>
+              </>
+          )}
+        </nav>
+      </header>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  adminUser: state.user.user.admin,
+  whileLoggedIn: state.user.logIn,
+});
+
+Header.propTypes = {
+  adminUser: PropTypes.bool,
+  whileLoggedIn: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  adminUser: false,
+  whileLoggedIn: false,
+};
+
+export default connect(mapStateToProps)(Header);
