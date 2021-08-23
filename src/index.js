e23fe5ecab9c363d6dfb75a0b17f697361dc72fb
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-23 07:46:04 -0600

    Set up index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import rootReducer from './reducers';
+import AppRouter from './routes/AppRouter';
+import { decode } from './api/api';
+import { setUser, logIn } from './actions/user';
+import { autoLogin } from './api/userAuth';
 import './css/index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = rootReducer;
+const body = (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
 );
+
+const runAutoLogin = async (userId) => {
+  const response = await autoLogin(userId);
+  if (response.logged_in) {
+    store.dispatch(setUser(response.user));
+    store.dispatch(logIn(true));
+  } else {
+    store.dispatch(logIn(false));
+    store.dispatch(setUser({}));
+    localStorage.clear();
+  }
+};
+
+if (localStorage.token) {
+  const decodedToken = decode(localStorage.token);
+  runAutoLogin(decodedToken.user_id).then(r => {});
+} else {
+  store.dispatch(logIn(false));
+}
+
+ReactDOM.render(body, document.getElementById('root'));
+
+
+
