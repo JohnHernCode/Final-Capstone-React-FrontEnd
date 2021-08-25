c48a6c5fc7cf6456ea90335f3927b836031de6f9
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-25 16:03:25 -0600

    Add url for heroku api

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000';
-// const baseUrl = 'https://capstone-api-johnhern.herokuapp.com';
+// const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://capstone-api-johnhern.herokuapp.com';
 
 export default baseUrl;
