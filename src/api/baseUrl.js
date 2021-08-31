fd821037cd741da0ec44beaa617e42f9a63fb5d0
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-31 09:43:11 -0600

    Test front end changes

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
-// const baseUrl = 'http://localhost:3000';
-const baseUrl = 'https://rails-capstone-api.herokuapp.com';
+const baseUrl = 'http://localhost:3000';
+// const baseUrl = 'https://rails-capstone-api.herokuapp.com';
 
 export default baseUrl;
--- src/helpers/restTracks.js
+++ src/helpers/restTracks.js
@@ -3,10 +3,10 @@ import { sendRequestWithData, sendRequestWithoutData } from '../api/api';
 export const getTracks = async () => sendRequestWithoutData('get', 'records')
   .then((response) => response.data).catch((error) => error);
 
-export const addNewTrack = async (result, itemId, date) => sendRequestWithData('post', 'records', { record: { result, itemId, date } })
+export const addNewTrack = async (result, itemId, date) => sendRequestWithData('post', 'records', { record: { result, item_id: itemId, date } })
   .then((response) => response).catch((error) => error);
 
-export const updateTrack = async (id, result, itemId, date) => sendRequestWithData('put', `records/${id}`, { record: { result, itemId, date } })
+export const updateTrack = async (id, result, itemId, date) => sendRequestWithData('put', `records/${id}`, { record: { result, item_id: itemId, date } })
   .then((response) => response).catch((error) => error);
 
 export const removeTrackFromDB = async (id) => sendRequestWithoutData('delete', `records/${id}`)
