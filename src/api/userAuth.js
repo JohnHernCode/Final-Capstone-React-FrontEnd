dfe95927fe43c2494063e6d369c968d835fd68ef
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-31 18:11:34 -0600

    Remove autologin

--- src/api/userAuth.js
+++ src/api/userAuth.js
@@ -5,6 +5,3 @@ export const signedUp = async (username, password) => sendRequestWithData('post'
 
 export const loggedIn = async (username, password) => sendRequestWithData('post', 'login', { user: { username, password } })
   .then((response) => response.data).catch((error) => error);
-
-export const autoLogin = async (userId) => sendRequestWithData('post', 'auto_login', { user: { user_id: userId } })
-  .then((response) => response.data).catch((error) => error);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import setStore from './store/setupStore';
-import { decode } from './api/api';
-import { setUser, logIn } from './actions/user';
-import { autoLogin } from './api/userAuth';
 import './css/index.css';
 import AppRoutes from './routes/AppRoutes';
 
@@ -14,24 +11,5 @@ const body = (
     <AppRoutes />
   </Provider>
 );
-
-const runAutoLogin = async (userId) => {
-  const response = await autoLogin(userId);
-  if (response.logged_in) {
-    store.dispatch(setUser(response.user));
-    store.dispatch(logIn(true));
-  } else {
-    store.dispatch(logIn(false));
-    store.dispatch(setUser({}));
-    localStorage.clear();
-  }
-};
-
-if (localStorage.token) {
-  const decodedToken = decode(localStorage.token);
-  runAutoLogin(decodedToken.user_id);
-} else {
-  store.dispatch(logIn(false));
-}
 
 ReactDOM.render(body, document.getElementById('root'));
